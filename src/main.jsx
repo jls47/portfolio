032833c3b8323e624dc90ed7f6080e86175e1e1f
jls47 <lukeschaefer@gmail.com>
2023-08-27 20:28:15 -0700

    first commit, redux store setup and getting component files together

---
+++ src/main.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider} from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(() => ({
+  activeWork: -1,
+  blogWriting: false,
+  works: [
+    {
+      id: 0,
+      imgsrc: "blank.jpg",
+      title: "Test",
+      desc: "Test work",
+      links: ["http://jlukes.com", "https://www.google.com/"]
+    },
+    {
+      id: 1,
+      imgsrc: "blank.jpg",
+      title: "Test",
+      desc: "Test work",
+      links: ["http://jlukes.com", "https://www.google.com/"]
+    }
+  ],
+  posts: [
+    {
+      id: 0,
+      title: "Test post",
+      body: "Test body",
+      comments: [
+        {
+          id: 0,
+          author: "Luke",
+          body: "Hi there"
+        },
+        {
+          id: 1,
+          author: "John",
+          body: "Hello"
+        }
+      ]
+    },
+    {
+      id: 1,
+      title: "Test post 2",
+      body: "Test body",
+      comments: [
+        {
+          id: 0,
+          author: "Luke",
+          body: "Hi there"
+        },
+        {
+          id: 1,
+          author: "John",
+          body: "Hello"
+        }
+      ]
+    }
+  ]
+  
+}));
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+)
