15c3da479dfdbf013d8e6c4c94dcc50e97b8888a
jls47 <lukeschaefer@gmail.com>
2024-10-12 17:44:41 -0700

    updated tech

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,30 +5,32 @@ import reactColor from './assets/reactlogo.svg'
 import viteLogo from '/vite.svg'
 import cppLogo from './assets/c++gray.svg'
 import cppColor from './assets/c++.svg'
-import goLogo from './assets/gogray.svg'
-import goColor from './assets/go-gopher.svg'
+import javaLogo from './assets/javablack.svg'
+import javaColor from './assets/javacolor.svg'
 import mongoLogo from './assets/mongodb.svg'
 import mongoColor from './assets/ogmongo.svg'
-import nodeLogo from './assets/nodejs-gray.svg'
-import nodeColor from './assets/nodejs-icon.svg'
+import TSLogo from './assets/typescriptblack.svg'
+import TSColor from './assets/typescriptcolor.svg'
 
 import './App.css'
+
+//https://leetcode.com/problems/3sum/
 
 function Sidebar() {
 
 	const imgsColor = [
 		reactColor,
-		nodeColor,
+		TSColor,
 		cppColor,
-		goColor,
+		javaColor,
 		mongoColor
 	]
 
 	const imgsGray = [
 		reactLogo,
-		nodeLogo,
+		TSLogo,
 		cppLogo,
-		goLogo,
+		javaLogo,
 		mongoLogo
 	]
 
@@ -62,11 +64,11 @@ function Sidebar() {
                     <a className="tech" onMouseEnter={() => {techHover(0)}} onMouseLeave={() => {techLeave(0)}}
                         href="https://reactjs.org/"><img className="techimg" src={imgRefs[0]}/></a>
                     <a className="tech" onMouseEnter={() => {techHover(1)}} onMouseLeave={() => {techLeave(1)}}
-                        href="https://nodejs.org/en/"><img className="techimg" src={imgRefs[1]}/></a>
+                        href="https://www.typescriptlang.org/"><img className="techimg" src={imgRefs[1]}/></a>
                     <a className="tech" onMouseEnter={() => {techHover(2)}} onMouseLeave={() => {techLeave(2)}}
                         href="https://www.cplusplus.com/"><img className="techimg" src={imgRefs[2]}/></a>
                     <a className="tech" onMouseEnter={() => {techHover(3)}} onMouseLeave={() => {techLeave(3)}}
-                        href="https://go.dev/"><img className="techimg" src={imgRefs[3]}/></a>
+                        href="https://www.java.com/en/"><img className="techimg" src={imgRefs[3]}/></a>
                     <a className="tech" onMouseEnter={() => {techHover(4)}} onMouseLeave={() => {techLeave(4)}}
                         href="https://www.mongodb.com/"><img className="techimg" src={imgRefs[4]}/></a>
 
